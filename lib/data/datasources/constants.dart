c3be2a391383686ee40a9a142279ea5644469d19
M. Yoga Nugraha <myoganugraha@gmail.com>
2022-08-17 12:06:01 +0800

    add type=movie to url

--- lib/data/datasources/constants.dart
+++ lib/data/datasources/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DatasourcesConstants {
   static String apikey = dotenv.env['omdb_dev_apikey']!;
-  static String baseURL = 'https://www.omdbapi.com/?apikey=$apikey';
+  static String baseURL = 'https://www.omdbapi.com/?apikey=$apikey&type=movie';
   static String searchMovieEndpoint = '$baseURL&s=';
 }
