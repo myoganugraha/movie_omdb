a0ef2968469096faff48f01476faeaee6868194d
M. Yoga Nugraha <myoganugraha@gmail.com>
2022-08-17 13:27:00 +0800

    change .env variable

--- lib/data/datasources/constants.dart
+++ lib/data/datasources/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DatasourcesConstants {
-  static String apikey = dotenv.env['omdb_dev_apikey']!;
+  static String apikey = dotenv.env['omdb_apikey']!;
   static String baseURL = 'https://www.omdbapi.com/?apikey=$apikey&type=movie';
   static String searchMovieEndpoint = '$baseURL&s=';
   static String movieDetailsEndpoint = '$baseURL&plot=short&i=';
