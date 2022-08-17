f2f650ebe40a343f8a917b1ea24b471dc66b23ac
M. Yoga Nugraha <myoganugraha@gmail.com>
2022-08-17 16:18:34 +0800

    add size constants

---
+++ lib/presentation/size_constants.dart
@@ -0,0 +1,14 @@
+class SizeConstants {
+  static const size_0 = 0.0;
+  static const size_0_9 = 0.9;
+  static const size_1 = 1.0;
+  static const size_2 = 2;
+  static const size_8 = 8.0;
+  static const size_10 = 10.0;
+  static const size_16 = 16.0;
+  static const size_18 = 18.0;
+  static const size_20 = 20.0;
+  static const size_24 = 24.0;
+  static const size_30 = 30.0;
+  static const size_40 = 40.0;
+}
